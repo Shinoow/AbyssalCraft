569792f0df2e0a9d10dbe5542e14532b2692ead5
Shinoow <felix@lundquist.ws>
2015-03-23 10:52:33 +0100

    Test 1/2
    This is a test

--- src/main/java/com/shinoow/abyssalcraft/api/recipe/package-info.java
+++
@@ -1,3 +0,0 @@
-@API(apiVersion="1.2.0",owner="abyssalcraft",provides="AbyssalCraftAPI")
-package com.shinoow.abyssalcraft.api.recipe;
-import cpw.mods.fml.common.API;
--- src/main/java/com/shinoow/abyssalcraft/api/render/package-info.java
+++
@@ -1,3 +0,0 @@
-@API(apiVersion="1.2.0",owner="abyssalcraft",provides="AbyssalCraftAPI")
-package com.shinoow.abyssalcraft.api.render;
-import cpw.mods.fml.common.API;
