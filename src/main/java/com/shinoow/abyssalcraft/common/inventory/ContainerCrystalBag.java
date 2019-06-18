034405ed5e322d8c9d0014e61e63077f44b324d3
Shinoow <felix@lundquist.ws>
2019-06-18 22:11:27 +0200

    Re-added Inventory Tweaks support for Crystal Bags

--- src/main/java/com/shinoow/abyssalcraft/common/inventory/ContainerCrystalBag.java
+++ src/main/java/com/shinoow/abyssalcraft/common/inventory/ContainerCrystalBag.java
@@ -14,15 +14,17 @@ package com.shinoow.abyssalcraft.common.inventory;
 import com.shinoow.abyssalcraft.api.APIUtils;
 import com.shinoow.abyssalcraft.common.items.ItemCrystalBag;
 
+import invtweaks.api.container.ChestContainer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.ClickType;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.Optional.Interface;
 
-//@Interface(iface = "invtweaks.api.container.ChestContainer", modid = "inventorytweaks")
-//@ChestContainer
+@Interface(iface = "invtweaks.api.container.ChestContainer", modid = "inventorytweaks")
+@ChestContainer
 public class ContainerCrystalBag extends Container
 {
 	/** The Item Inventory for this Container */
