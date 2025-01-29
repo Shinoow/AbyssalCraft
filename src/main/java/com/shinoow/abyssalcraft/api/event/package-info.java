f66267f5002874520f581ae6ea959cbc76a5610f
Shinoow <github@shinoow.com>
2025-01-29 14:45:44 +0100

    2024 -> 2025

--- src/main/java/com/shinoow/abyssalcraft/api/event/package-info.java
+++ src/main/java/com/shinoow/abyssalcraft/api/event/package-info.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * AbyssalCraft
+ * Copyright (c) 2012 - 2025 Shinoow.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Contributors:
+ *     Shinoow -  implementation
+ ******************************************************************************/
 @API(apiVersion=AbyssalCraftAPI.API_VERSION,owner="abyssalcraft",provides="AbyssalCraftAPI|Event")
 package com.shinoow.abyssalcraft.api.event;
 import com.shinoow.abyssalcraft.api.AbyssalCraftAPI;
